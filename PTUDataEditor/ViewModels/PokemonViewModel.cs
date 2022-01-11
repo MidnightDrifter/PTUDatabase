42433d699435e9f680b6980150f3af1806ab307d
MidnightDrifter <6774070+MidnightDrifter@users.noreply.github.com>
2022-01-11 17:51:25 -0600

    Creation of Pokemon and PokemonViewModel class.  Neither is fully implemented, since I'm not entirely sure how the various parts of the Pokemon Moves (Moves, Move Range, Move Category + effects, etc) are all linked together.

---
+++ PTUDataEditor/ViewModels/PokemonViewModel.cs
@@ -0,0 +1,49 @@
+﻿using PTUDatabase;
+
+namespace PTUDataEditor.ViewModels
+{
+    public class PokemonViewModel : ViewModelBase
+    {
+        public Pokemon Model
+        {
+            get => new()
+            {
+
+                SpeciesName = SpeciesName,
+                CharacterName = CharacterName,
+                PokemonTypes = PokemonTypes,
+                Nature = Nature,
+                Forms = Forms,
+                Capabilities = Capabilities,
+                Abilities = Abilities,
+                Stats = Stats,
+                Level = Level,
+                Skills = Skills
+
+                //Moves, Edges (?), and Held Items still need added
+
+            };
+            private set
+            {
+                SpeciesName = value.SpeciesName;
+                CharacterName = value.CharacterName;
+                PokemonTypes = value.PokemonTypes;
+                Nature = value.Nature;
+                Forms = value.Forms;
+                Capabilities = value.Capabilities;
+                Abilities = value.Abilities;
+                Stats = value.Stats;
+                Level = value.Level;
+                Skills = value.Skills;
+            }
+        }
+
+
+        public PokemonViewModel(Pokemon model)
+        {
+            Model = model;
+        }
+
+
+    }
+}
---
+++ PTUDatabase/Classes/Pokemon.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTUDatabase.Classes
+{
+    public record Pokemon
+    {
+        /*
+         * Pokmeon have:
+         * Species name
+         * Regular / given name?
+         * Movelist
+         * Type
+         * Nature
+         * Forms
+         * Capabilities
+         * Abilities
+         * Stat block
+         * Level + current XP
+         * Skills
+         * Edges?
+         * Held Item?
+         */
+
+        public Species SpeciesName { get; init; }
+        public String CharacterName { get; init; } = "Nugget Boi";
+
+        public IList<PokemonType> PokemonTypes { get; init; } = Array.Empty<PokemonType>();
+
+
+        //Not sure how Yota is tying Moves + Move Requirements + Move Ranges (+ Contest Effects??) together, skipping for now
+
+
+        public Nature Nature { get; init; } = Nature.Sassy;
+
+        public Form Forms { get; init; }  //Initialize to NULL?
+
+        public Capabilities Capabilities { get; init; }  //These get pulled from the CSV?
+
+        public IList<Ability> Abilities { get; init; } = Array.Empty<Ability>();
+
+        public Stats Stats { get; init; }  //Get pulled from CSV / YAML
+
+        public int Level { get; set; }
+
+        public Skills Skills { get; init; } = Skills.Minimum;
+
+        //Edges and Held Items yet to be implemented?
+
+    }
+}
